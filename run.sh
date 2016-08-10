fc2f810980dc67785a3a7fb40e6cb1d2f8305ac3
Pedro Bruel <pedro.bruel@gmail.com>
2016-08-09 22:59:12 -0300

    Change experiments script.

--- run.sh
+++ run.sh
@@ -2,12 +2,29 @@
 
 name="dfadd"
 duration=5400
-parallel=4
-workers=4
+parallel=2
+workers=2
 seed="seed.json"
-async="apply"
+async="map"
 
-sudo python2 tuner.py --stop-after=$duration --results-log=log.txt --parallelism=$parallel --processes=$workers --seed-configuration="$seed" --no-dups --quiet --no-wait
+sudo python2 tuner.py --stop-after=$duration --results-log=log.txt --parallelism=$parallel --processes=$workers --seed-configuration="$seed" --no-dups --quiet
+
+new_dir=$name\_$duration\_p$parallel\_w$workers\_$async
+
+mkdir $new_dir
+
+mv log.txt opentuner.log best_log.txt best_log.json $new_dir
+
+sudo ./clean.sh
+
+name="dfadd"
+duration=5400
+parallel=6
+workers=6
+seed="seed.json"
+async="map"
+
+sudo python2 tuner.py --stop-after=$duration --results-log=log.txt --parallelism=$parallel --processes=$workers --seed-configuration="$seed" --no-dups --quiet
 
 new_dir=$name\_$duration\_p$parallel\_w$workers\_$async
 
