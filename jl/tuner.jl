fe5a3f4810e5ef6c98cd0de2caf5b38a95e51b5e
Pedro Bruel <pedro.bruel@gmail.com>
2016-08-12 23:26:09 -0300

    Add new tuner version and experiments results.

--- jl/tuner.jl
+++ jl/tuner.jl
@@ -4,14 +4,42 @@ import JSON
     using StochasticSearch
 end
 
-json_parameters = JSON.parsefile("legup_parameters.json"; dicttype=Dict)
+function load_parameters()
+    json_parameters = JSON.parsefile("legup_parameters.json"; dicttype=Dict)
 
-parameters = []
+    parameters::Array{Parameter, 1}
+    parameters = []
 
-print(typeof(json_parameters))
-
-print(json_parameters["legup_parameters"])
-
-for parameter in json_parameters.second
-    print(parameter)
+    for parameter in json_parameters["legup_parameters"]
+        p_type = parameter["type"]
+        if p_type == "int"
+            push!(parameters, IntegerParameter(parse(Int, parameter["min"]),
+                                               parse(Int, parameter["max"]),
+                                               parse(Int, parameter["default"]),
+                                               parameter["name"]))
+        elseif p_type == "bool"
+            value = false
+            if parameter["default"] == "True"
+                value = true
+            end
+            push!(parameters, BoolParameter(value,
+                                            parameter["name"]))
+        elseif p_type == "Enum"
+            values = []
+            for val in split(parameter["values"])
+                push!(values, StringParameter(val, val))
+            end
+            push!(parameters, EnumParameter(values,
+                                            findfirst(split(parameter["values"]),
+                                                      parameter["default"]),
+                                            parameter["name"]))
+        else
+            println("ERROR: No such type")
+            exit()
+        end
+    end
+    return parameters
 end
+
+parameters    = load_parameters()
+configuration = Configuration(parameters, "legup_parameters")
--- run.sh
+++ run.sh
@@ -2,27 +2,10 @@
 
 name="dfadd"
 duration=5400
-parallel=2
-workers=2
+parallel=1
+workers=1
 seed="seed.json"
-async="map"
-
-sudo python2 tuner.py --stop-after=$duration --results-log=log.txt --parallelism=$parallel --processes=$workers --seed-configuration="$seed" --no-dups --quiet
-
-new_dir=$name\_$duration\_p$parallel\_w$workers\_$async
-
-mkdir $new_dir
-
-mv log.txt opentuner.log best_log.txt best_log.json $new_dir
-
-sudo ./clean.sh
-
-name="dfadd"
-duration=5400
-parallel=6
-workers=6
-seed="seed.json"
-async="map"
+async="seq"
 
 sudo python2 tuner.py --stop-after=$duration --results-log=log.txt --parallelism=$parallel --processes=$workers --seed-configuration="$seed" --no-dups --quiet
 
