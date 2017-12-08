332beb440b7aa000be170739b94c6f890a945417
phrb <pedro.bruel@gmail.com>
2017-12-08 10:44:00 +0100

    Restructure directories

--- post_place_and_route/py/results/r_scripts/load_data.r
+++ post_place_and_route/py/results/r_scripts/load_data.r
@@ -5,10 +5,15 @@ library(jsonlite)
 
 runs         <- 10
 tuning_time  <- 5400
+
 repository   <- "~/code/legup-tuner"
 results      <- "post_place_and_route/py/results"
+
+output_dir   <- "data"
+
 experiments  <- c("default_stratixV_perf", "default_stratixV_perflat",
                   "default_stratixV_area", "default_stratixV_balanced")
+
 applications <- c("dfadd", "dfdiv", "dfmul", "sha", "motion", "adpcm",
                   "dfsin", "aes", "blowfish", "gsm", "mips")
 
@@ -23,6 +28,17 @@ json_configurations <- "best_log.json"
 headers <- c("WNS", "Cycles", "FMax", "LUs", "Pins", "Regs", "Blocks", "RAM",
              "DPS")
 
+#
+# This function merges columns and fills missing rows with 'NA'.
+# This happens in this dataset because of unsynchronized logging,
+# where the last configuration was saved twice in one of the log
+# files. Will require to later clean the lines with 'NA's.
+#
+# Function from:
+#
+#    https://stackoverflow.com/questions/7962267/cbind-a-df-with-an-empty-df-cbind-fill
+#
+#
 cbind.fill <- function(...){
     nm <- list(...)
     nm <- lapply(nm, as.matrix)
@@ -31,17 +47,19 @@ cbind.fill <- function(...){
         rbind(x, matrix(, n-nrow(x), ncol(x)))))
 }
 
+dir.create(output_dir)
+
 for (experiment in experiments) {
-    dir.create(strsplit(experiment, "_")[[1]][3])
+    dir.create(paste(output_dir, strsplit(experiment, "_")[[1]][3], sep = "/"))
 
     for (application in applications) {
         data <- data.frame()
 
         for (iteration in 1:runs) {
             target_file <- (paste(repository, results, experiment,
-                                  paste(application, tuning_time,
-                                        iteration, sep = "_"),
-                                  json_configurations, sep = "/"))
+                                  paste(application, tuning_time, iteration,
+                                        sep = "_"), json_configurations,
+                                  sep = "/"))
 
             if (file.exists(target_file)) {
                 configuration <- fromJSON(target_file)
@@ -52,7 +70,8 @@ for (experiment in experiments) {
             for (measurement in txt_measurements) {
                 target_file <- paste(repository, results, experiment,
                                      paste(application, tuning_time, iteration,
-                                           sep = "_"), measurement, sep = "/")
+                                           sep = "_"), measurement,
+                                     sep = "/")
 
                 if (file.exists(target_file)) {
                     new_column <- read.table(target_file, header = FALSE)[2]
@@ -73,13 +92,17 @@ for (experiment in experiments) {
                 if (nrow(data) == 0) {
                     data <- columns
                 } else {
-                    data = bind_rows(as.data.frame(data), as.data.frame(columns))
+                    data = bind_rows(as.data.frame(data),
+                                     as.data.frame(columns))
                 }
             }
         }
 
-        write.csv(data, file = paste(paste(strsplit(experiment, "_")[[1]][3],
-                                                    application, sep = "/"), ".csv",
+        data <- data[complete.cases(data), ]
+
+        write.csv(data, file = paste(paste(output_dir, strsplit(experiment,
+                                                                "_")[[1]][3],
+                                           application, sep = "/"), ".csv",
                                      sep = ""))
     }
 }
