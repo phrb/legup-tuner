60be51ca6eb3c372b5a19bbe871e1058f9ca4a24
phrb <pedro.bruel@gmail.com>
2017-12-11 10:56:58 +0100

    Plot correlations for all applications

--- post_place_and_route/py/results/r_scripts/correlations.r
+++ post_place_and_route/py/results/r_scripts/correlations.r
@@ -1,7 +1,6 @@
 library(dplyr)
 
 setEPS()
-postscript("correlations.eps", width = 16, height = 11)
 
 #
 # Function adapted from:
@@ -21,6 +20,9 @@ sorted_correlations <- function(data, datapoints) {
     hardware_metrics <- c("WNS", "Cycles", "FMax", "LUs", "Pins", "Regs",
                           "Blocks", "RAM", "DPS")
 
+    #
+    # Restricting comparisons to correlations that contain a hardware metric
+    #
     fm <- dplyr::filter(fm, grepl(paste(hardware_metrics, collapse = "|"),
                                   fm$Second.Variable))
 
@@ -31,54 +33,86 @@ csv_dir <- c("~/code/legup-tuner/",
              "post_place_and_route/py/results/r_scripts/",
              "data")
 
+plot_dir <- c("~/code/legup-tuner/",
+             "post_place_and_route/py/results/r_scripts/",
+             "correlations")
+
 experiments <- c("balanced", "area", "perf", "perflat")
 
 applications <- c("dfadd", "dfdiv", "dfmul", "sha", "motion", "adpcm",
                   "dfsin", "aes", "blowfish", "gsm", "mips")
 
-data       <- data.frame()
-clean_data <- data.frame()
+plot_application_correlations <- function() {
+    dir.create(paste(plot_dir, collapse = ""))
 
-application <- applications[2]
+    for (application in applications) {
+        data       <- data.frame()
+        clean_data <- data.frame()
 
-for (experiment in experiments) {
-    new_data <- read.csv(paste(paste(csv_dir, collapse = ""),
-                               experiment, paste(application,
-                                                 ".csv",
-                                                 sep = ""),
-                               sep = "/"),
-                         header = TRUE, sep = ",")
+        for (experiment in experiments) {
+            new_data <- read.csv(paste(paste(csv_dir, collapse = ""),
+                                       experiment, paste(application,
+                                                         ".csv",
+                                                         sep = ""),
+                                       sep = "/"),
+                                 header = TRUE, sep = ",")
 
-    new_data <- as.data.frame(new_data)
-    new_data <- new_data[is.finite(new_data$WNS),]
+            new_data <- as.data.frame(new_data)
+            new_data <- new_data[is.finite(new_data$WNS),]
 
-    if (ncol(data) == 0) {
-        data <- new_data
-    } else {
-        data <- rbind(data, new_data)
+            if (ncol(data) == 0) {
+                data <- new_data
+            } else {
+                data <- rbind(data, new_data)
+            }
+        }
+
+        names <- c("WNS","RAM")
+        idx   <- match(names, names(data))
+        data  <- sapply(data, as.numeric)
+
+        correlation <- sorted_correlations(data, 120)
+
+        print(paste("Generating 120 strongest correlations for '", application,
+                    "'...", sep = ""))
+
+        write.csv(correlation, file = paste(paste(plot_dir, collapse = ""),
+                                            paste("correlations_", application,
+                                                  ".csv", sep = ""), sep = "/"))
+        print(paste("Generating scatter spots of the 30 strongest correlations for '",
+                    application, "'...", sep = ""))
+
+        print(paste(paste("CSV generated at ", plot_dir, collapse = "",
+                          sep = ""), paste("correlations_", application,
+                                           ".csv", sep = ""),
+                    sep = "/"))
+
+        short_correlation <- correlation[1:30, ]
+
+        postscript(paste(paste(plot_dir, collapse = ""), paste("correlations_",
+                                                               application,
+                                                               ".eps",
+                                                               sep = ""),
+                         sep = "/"),
+                   width = 16, height = 11)
+
+        old.par <- par(mfrow = c(5, 6))
+
+        for (i in 1:nrow(short_correlation)) {
+            first  <- as.character(short_correlation[i, 'First.Variable'])
+            second <- as.character(short_correlation[i, 'Second.Variable'])
+
+            plot(data[, first], data[, second], xlab = first, ylab = second)
+        }
+
+        print(paste(paste("Plot generated at ", plot_dir, collapse = "",
+                          sep = ""), paste("correlations_", application,
+                                           ".eps", sep = ""),
+                    sep = "/"))
+
+        par(old.par)
+        dev.off()
     }
 }
 
-names <- c("WNS","RAM")
-idx   <- match(names, names(data))
-data  <- sapply(data, as.numeric)
-
-correlation <- sorted_correlations(data, 120)
-
-print("120 Largest Correlations:")
-#print(correlation)
-
-print("Scatter Plots of the 10 Largest Correlations")
-short_correlation <- correlation[1:30, ]
-
-old.par <- par(mfrow = c(5, 6))
-
-for (i in 1:nrow(short_correlation)) {
-    first  <- as.character(short_correlation[i, 'First.Variable'])
-    second <- as.character(short_correlation[i, 'Second.Variable'])
-
-    plot(data[, first], data[, second], xlab = first, ylab = second)
-}
-
-par(old.par)
-dev.off()
+plot_application_correlations()
